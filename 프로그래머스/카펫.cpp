f4a92b2081469f36968ee4eef7bf3828e81cfb8e
gyuZzang <tndud0606@gmail.com>
2020-07-23 18:35:32 +0900

    Update 카펫.cpp

--- 프로그래머스/카펫.cpp
+++ 프로그래머스/카펫.cpp
@@ -7,7 +7,7 @@ vector<int> solution(int brown, int yellow) {
 	vector<int> answer;
 	int i,q;
 	brown = brown / 2 - 2; //= w+h of yellow
-	for (i = 1; i <= yellow/2+1; i++) {
+	for (i = 1; i <= yellow; i++) {
 		if (yellow%i == 0 && yellow/i+i==brown) {
 			answer.push_back(yellow / i+2);
 			answer.push_back(i+2);
