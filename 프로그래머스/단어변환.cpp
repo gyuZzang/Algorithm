dab8651babc2dde79f68a4d65bdbd8255772cd9b
gyuZzang <tndud0606@gmail.com>
2020-07-24 04:11:22 +0900

    Create 단어변환.cpp

---
+++ 프로그래머스/단어변환.cpp
@@ -0,0 +1,105 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+vector<int> visited;//방문 한 노드인지 표시
+stack<string> st;
+
+int count_diff(string a, string b) {//두 스트링에서 서로 몇 개의 알파벳이 다른가?
+	if (a.size() != b.size()) return -1;
+	int differ_cnt = 0;
+
+	for (int i = 0; i < a.size(); i++) {
+		if (a.c_str()[i] != b.c_str()[i])
+			differ_cnt++;
+	}
+	
+	return differ_cnt;
+}
+
+int dfs(string begin, string target, vector<string>words, int now_diff) {
+	//전체 반복...dfs 써야겠다
+	int i;
+
+	for (i = 0; i < words.size(); i++) {
+		//두 단어의 알파벳 차이=1 && 이전단계-target차이보다 현재i-targe차이가 작거나,
+		if (1 == count_diff(begin, words[i]) && now_diff > count_diff(target, words[i]) && visited[i] == 0) {
+			//now 업데이트(스택 추가), 방문 했다는 표시(1)
+			now_diff = count_diff(target, words[i]);
+			begin = words[i];
+			st.push(words[i]);
+			cout << "push " << words[i] << endl;
+			visited[i] = 1;
+			break;
+		}
+	}
+	if (i == words.size()) {//다 돌았는데 차이 작은게 없으면 같은걸로
+		for (i = 0; i < words.size(); i++) {
+			//두 단어의 알파벳 차이=1 && 이전단계-target차이보다 현재i-targe차이가 작은거 없으면 같아야함.
+			if (1 == count_diff(begin, words[i]) && now_diff == count_diff(target, words[i]) && visited[i] == 0) {
+				//now 업데이트(스택 추가), 방문 했다는 표시(1)
+				now_diff = count_diff(target, words[i]);
+				begin = words[i];
+				st.push(words[i]);
+				cout <<"push " <<words[i]<<endl;
+				visited[i] = 1;
+				break;
+			}
+		}
+	}
+
+	if (begin == target) {
+		int answer = st.size();
+		return answer;
+	}
+	
+
+	//다 찾았는데 target도 아니고 갈 곳 없다! ==> 다른 길 있을때까지 스택 pop, begin 재설정
+	if (i == words.size()) {
+		cout <<"pop " <<st.top()<<endl;
+		st.pop();
+		while (!st.empty()) {
+			begin = st.top();
+			for (i = 0; i < words.size(); i++) {//더 나아갈 곳이 있니?
+				if (1 == count_diff(begin, words[i]) && now_diff >= count_diff(target, words[i]) && visited[i] == 0) {
+					return dfs(begin, target, words, now_diff);
+				}
+			}
+			if (i == words.size()) {
+				cout << "pop " << st.top() << endl;
+				st.pop();//없으면 pop
+			}
+		}
+	}
+	else return dfs(begin, target, words, now_diff);
+
+}
+
+
+int solution(string begin, string target, vector<string> words) {
+	int answer = 0;
+	int i;
+	int now_diff = count_diff(begin, target);
+	visited.resize(words.size(),0);
+
+	//if (find(words.begin(), words.end(), target) == words.end()) return 0;
+	for (i = 0; i < words.size(); i++) {
+		if (words[i] == target) break;
+	}
+
+	if (i == words.size()) return 0;
+
+
+	answer=dfs(begin, target, words, now_diff);
+
+
+	return answer;
+}
+
+int main() {
+	cout << endl<<solution("hit", "cog", { "hot", "dot", "dog", "lot", "log", "cog" }) << endl;
+	cout << endl << solution("hit", "cog", { "hot","dot","dog","lot","log" }) << endl;
+	system("pause");
+}
