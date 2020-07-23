2250736a27f39ba9da4afb83c9f7865523c2deb0
gyuZzang <tndud0606@gmail.com>
2020-07-23 18:33:11 +0900

    Create 카펫.cpp

---
+++ 프로그래머스/카펫.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+	vector<int> answer;
+	int i,q;
+	brown = brown / 2 - 2; //= w+h of yellow
+	for (i = 1; i <= yellow/2+1; i++) {
+		if (yellow%i == 0 && yellow/i+i==brown) {
+			answer.push_back(yellow / i+2);
+			answer.push_back(i+2);
+			break;
+		}
+	}
+
+	return answer;
+}
